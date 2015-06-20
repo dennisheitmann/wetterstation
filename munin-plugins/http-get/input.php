9986f24b4208c1bb6990efd5b8459ee6d33fcab1
dennisheitmann <dennis@dennisheitmann.de>
2015-06-21 00:13:24 +0200

    Cleanup HTTP GET (Push)

--- munin-plugins/http-get/input.php
+++ munin-plugins/http-get/input.php
@@ -4,45 +4,55 @@
 
 $data = strip_tags($_GET['data']);
 
+// Per GET erhaltene Daten in Dateien schreiben
 if ($data) {
+  $t1_C = 0;
+  $t2_C = 0;
+  $t3_C = 0;
+  $mbar = 0;
+  $humi = 0;
+  $mVcc = 0;
+  $BVcc = 0;
+  $SVcc = 0;
   $data = str_replace('=', ' ', $data);
   $data = explode(',', $data);
   foreach ($data as $zeile) {
+    global $t1_C, $t2_C, $t3_C, $mbar, $humi, $mVcc, $BVcc, $SVcc;
     if (strstr($zeile, 't1_C.value')) {
-      file_put_contents("/tmp/wetterstation-temperatur1-value", $zeile."\n");
-      $t1_C = str_replace('t1_C.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-temperatur1-value', $zeile."\n");
+      $t1_C = str_replace('t1_C.value ', '', $zeile);
     }
     else if (strstr($zeile, 't2_C.value')) {
-      file_put_contents("/tmp/wetterstation-temperatur2-value", $zeile."\n");
-      $t2_C = str_replace('t2_C.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-temperatur2-value', $zeile."\n");
+      $t2_C = str_replace('t2_C.value ', '', $zeile);
     }
     else if (strstr($zeile, 't3_C.value')) {
-      file_put_contents("/tmp/wetterstation-temperatur3-value", $zeile."\n");
-      $t3_C = str_replace('t3_C.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-temperatur3-value', $zeile."\n");
+      $t3_C = str_replace('t3_C.value ', '', $zeile);
     }
     else if (strstr($zeile, 'mbar.value')) {
-      file_put_contents("/tmp/wetterstation-luftdruck-value", $zeile."\n");
-      $mbar = str_replace('mbar.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-luftdruck-value', $zeile."\n");
+      $mbar = str_replace('mbar.value ', '', $zeile);
     }
     else if (strstr($zeile, 'hum%.value')) {
-      file_put_contents("/tmp/wetterstation-luftfeuchte-value", $zeile."\n");
-      $humi = str_replace('hum%.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-luftfeuchte-value', $zeile."\n");
+      $humi = str_replace('hum%.value ', '', $zeile);
     }
     else if (strstr($zeile, 'mVcc.value')) {
-      file_put_contents("/tmp/wetterstation-spannung-value", $zeile."\n");
-      $mVcc = str_replace('mVcc.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-spannung-value', $zeile."\n");
+      $mVcc = str_replace('mVcc.value ', '', $zeile);
     }
     else if (strstr($zeile, 'BVcc.value')) {
-      file_put_contents("/tmp/wetterstation-akku-value", $zeile."\n");
-      $BVcc = str_replace('BVcc.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-akku-value', $zeile."\n");
+      $BVcc = str_replace('BVcc.value ', '', $zeile);
     }
     else if (strstr($zeile, 'SVcc.value')) {
-      file_put_contents("/tmp/wetterstation-solar-value", $zeile."\n");
-      $SVcc = str_replace('SVcc.value', '', $zeile);
+      file_put_contents('/tmp/wetterstation-solar-value', $zeile."\n");
+      $SVcc = str_replace('SVcc.value ', '', $zeile);
     }
   }
   $t0_C = min($t1_C, $t2_C, $t3_C);
-  file_put_contents("/tmp/wetterstation-temperatur_min-value", "t0_C.value".$t0_C."\n");
+  file_put_contents('/tmp/wetterstation-temperatur_min-value', 't0_C.value '.$t0_C."\n");
 }
 
 ?>
